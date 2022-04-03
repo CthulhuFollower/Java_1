c2831112e862b9d960dffc34d8c2e2d425a062de
Julian110101 <57191513+IngenieroJulian@users.noreply.github.com>
2022-04-03 14:08:49 -0500

    Update main.java

--- Java_Act9/java_act8aux/main.java
+++ Java_Act9/java_act8aux/main.java
@@ -21,45 +21,50 @@ public class main {
     }
 
     static void showMenu(Deck d) {
-        int seleccion;
+        int seleccion = 0;
         do {
-            System.out.println("Indique una opcion: \n"
-                    + "1.- Crear una nuevo deck \n"
-                    + "2.- Barajear el deck \n"
-                    + "3.- Tomar una carta de hasta arriba \n"
-                    + "4.- Tomar una carta aleatoria \n"
-                    + "5.- Tomar una mano (5 cartas) \n"
-                    + "6.- Terminar programa");
-            seleccion = entrada.nextInt();
-            switch (seleccion) {
-                case 1:
-                    d.crearBaraja();
-                    System.out.println("");
-                    break;
-                case 2:
-                    d.shuffle();
-                    System.out.println("");
-                    break;
-                case 3:
-                    d.head();
-                    System.out.println("");
-                    break;
-                case 4:
-                    d.pick();
-                    System.out.println("");
-                    break;
-                case 5:
-                    d.hand();
-                    System.out.println("");
-                    break;
-                case 6:
-                    System.out.println("Gracias por usar el codigo");
-                    System.out.println("");
-                    break;
-                default:
-                    System.out.println("Opcion invalida");
-                    System.out.println("");
-                    break;
+            try {
+                System.out.println("Indique una opcion: \n"
+                        + "1.- Crear una nuevo deck \n"
+                        + "2.- Barajear el deck \n"
+                        + "3.- Tomar una carta de hasta arriba \n"
+                        + "4.- Tomar una carta aleatoria \n"
+                        + "5.- Tomar una mano (5 cartas) \n"
+                        + "6.- Terminar programa");
+                seleccion = Integer.parseInt(entrada.nextLine());
+                switch (seleccion) {
+                    case 1:
+                        d.crearBaraja();
+                        System.out.println("");
+                        break;
+                    case 2:
+                        d.shuffle();
+                        System.out.println("");
+                        break;
+                    case 3:
+                        d.head();
+                        System.out.println("");
+                        break;
+                    case 4:
+                        d.pick();
+                        System.out.println("");
+                        break;
+                    case 5:
+                        d.hand();
+                        System.out.println("");
+                        break;
+                    case 6:
+                        System.out.println("Gracias por usar el codigo");
+                        System.out.println("");
+                        break;
+                    default:
+                        System.out.println("Opcion invalida");
+                        System.out.println("");
+                        break;
+                }
+
+            } catch (Exception e) {
+                System.out.println("Opcion invalida");
             }
         } while (seleccion != 6);
     }
